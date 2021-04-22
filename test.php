8bc6943b602c6f0fffd441d67ffa938a4422e77d
Yannick Armspach <yannick.armspach@gmail.com>
2021-04-22 17:04:08 +0200

    Update test.php

--- test.php
+++ test.php
@@ -8,13 +8,13 @@ class Test {
     function __construct( array $data ) {}
 
     //test 2: Loop all data and Capitilize the title
-    function formatTitle() {}
+    private function formatTitle() {}
 
     //test 2: Loop all data and get the first 20 charactere
-    function formatContent() {}
+    private function formatContent() {}
 
     //test 2: Loop all data and add link to the array like 'http://localhost:8888/codetest/{id}'
-    function addLink() {}
+    private function addLink() {}
 
     //test 4: Go to the index.php and create a list with 
     public function get() 
